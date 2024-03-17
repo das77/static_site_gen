39ab6af4d02e47f0ffbb644eebf1b2ee54e02653
dspera <dspera@gmail.com>
2024-03-17 14:16:17 -0400

    added tests to textnode

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -1,11 +1,12 @@
 import unittest
 
 from textnode import TextNode
+from main import main
 
 class TestTextNode(unittest.TestCase):
     def test_eq(self):
-        node = TextNode("This is a text node", "bold")
-        node2 = TextNode("This is a text node", "bold")
+        node = TextNode("This is a text node", "bold", "https://example.com")
+        node2 = TextNode("This is a text node", "bold", "https://example.com")
         self.assertEqual(node, node2)
 
     def test_different_text(self):
