cb97c7c421d55391b52f1a39ef927619cc210307
das77 <dspera@gmail.com>
2024-03-17 13:54:58 -0400

    added tests

---
+++ src/main.py
@@ -0,0 +1,8 @@
+from textnode import TextNode
+
+def main():
+    txt_node = TextNode('this is a test', 'italic', 'https://dspera.com')
+    print(txt_node)
+
+if __name__ == '__main__':
+    main()
---
+++ src/test_textnode.py
@@ -0,0 +1,43 @@
+import unittest
+
+from textnode import TextNode
+
+class TestTextNode(unittest.TestCase):
+    def test_eq(self):
+        node = TextNode("This is a text node", "bold")
+        node2 = TextNode("This is a text node", "bold")
+        self.assertEqual(node, node2)
+
+    def test_different_text(self):
+        node1 = TextNode("Hello", "header", "https://example.com")
+        node2 = TextNode("World", "header", "https://example.com")
+        self.assertNotEqual(node1, node2)
+
+    def test_different_text_type(self):
+        node1 = TextNode("Hello", "header", "https://example.com")
+        node2 = TextNode("Hello", "paragraph", "https://example.com")
+        self.assertNotEqual(node1, node2)
+
+    def test_different_url(self):
+        node1 = TextNode("Hello", "header", "https://example.com")
+        node2 = TextNode("Hello", "header", "https://another-example.com")
+        self.assertNotEqual(node1, node2)
+
+    def test_none_url(self):
+        node1 = TextNode("Hello", "header", "https://example.com")
+        node2 = TextNode("Hello", "header", None)
+        self.assertNotEqual(node1, node2)
+
+    def test_none_text_type(self):
+        node1 = TextNode("Hello", "header", "https://example.com")
+        node2 = TextNode("Hello", None, "https://example.com")
+        self.assertNotEqual(node1, node2)
+
+    def test_none_url_and_text_type(self):
+        node1 = TextNode("Hello", "header", "https://example.com")
+        node2 = TextNode("Hello", None, None)
+        self.assertNotEqual(node1, node2)
+
+
+if __name__ == "__main__":
+    unittest.main()
